51d8615dfb18633b7c2e94fddb7835af53c49244
StefanCholakov <stcholakov98@gmail.com>
2021-01-13 16:11:29 -0800

    Added floor statistics

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,52 +1,48 @@
 import React, { Component } from "react";
-import {Chart} from "chart.js";
+import { Chart } from "chart.js";
 
 class LineChart extends Component {
-    constructor(props) {
-      super(props);
-      this.canvasRef = React.createRef();
-    }
-  
-    componentDidUpdate() {
-      this.myChart.data.labels = this.props.data.map(d => d.time);
-      this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
-      this.myChart.update();
-    }
-  
-    componentDidMount() {
-      this.myChart = new Chart(this.canvasRef.current, {
-        type: 'line',
-        options: {
-          maintainAspectRatio: false,
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.myChart.data.labels = this.props.data.labels;
+    this.myChart.data.datasets = this.props.data.datasets;
+    this.myChart.update();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.canvasRef.current, {
+      type: "line",
+      options: {
+        title: {
+          display: true,
+          text: "Заетост на етажите по часове",
+        },
+        maintainAspectRatio: false,
         scales: {
           yAxes: [
             {
               ticks: {
                 min: 0,
-                max: 100
-              }
-            }
-          ]
-        }
+                max: 100,
+              },
+            },
+          ],
+        },
       },
-        data: {
-          labels: this.props.data.map(d => d.time),
-          datasets: [{
-            label: this.props.title,
-            data: this.props.data.map(d => d.value),
-            fill: 'none',
-            backgroundColor: this.props.color,
-            pointRadius: 2,
-            borderColor: this.props.color,
-            borderWidth: 1,
-            lineTension: 0
-          }]
-        }
-      });
-    }
-  
-    render() {
-      return <canvas ref={this.canvasRef} />;
-    }
+
+      data: {
+        labels: this.props.data.labels,
+        datasets: this.props.data.datasets,
+      },
+    });
   }
+
+  render() {
+    return <canvas ref={this.canvasRef} />;
+  }
+}
 export default LineChart;
